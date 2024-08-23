b3fdbfc51e2e609db54553f33350f88b2f4049f5
Adil Khan <Adil@Adils-MacBook-Pro.local>
2024-08-23 14:47:12 -0400

    make front.js and style it

---
+++ my-app/src/components/Front.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default function Front(){
+  return(
+    <div className="front-page">
+      <h1>Quiz Game</h1>
+      <h2>You will be given 5 trivia questions. Try to get them all correct!</h2>
+      <button className="front-page-button">Start Quiz</button>
+    </div>
+  )
+}
