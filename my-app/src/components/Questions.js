2ceb22d23ade94f235c0ade218075ac0620e5c6c
Adil Khan <Adil@Adils-MacBook-Pro.local>
2024-08-23 16:21:27 -0400

    work with ap

--- my-app/src/components/Questions.js
+++ my-app/src/components/Questions.js
@@ -1,6 +1,19 @@
 import React from "react";
 
 export default function Questions() {
+  const [question, setQuestion] = React.useState({
+    quest: "what is pakistan",
+    answers: ["A", "B", "C", "D"]
+  })
+  const [allQuestions, setAllQuestions] = React.useState([])
+  
+  React.useEffect(() => {
+    fetch("https://opentdb.com/api.php?amount=5")
+      .then(res => res.json())
+      .then(data => setAllQuestions(data))
+  }, [])
+
+  console.log(allQuestions)
   return(
     <div>
       <p>Questipns</p>
